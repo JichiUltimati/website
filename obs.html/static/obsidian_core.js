7c21fbb82c81887e05cdb29f75e536668026379f
JichiUltimati <aly-bora.ulusoy@polytechnique.edu>
2023-01-01 22:50:02 +0100

    fix last commit problem

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 1;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '72262632735016429577757213524323840015'                       // used to check whether the localStorage data is stale
+var gzip_hash = '162022756360803858761986137552198235407'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
