d358dab6f6e71efac8a540280e06748329333576
JichiUltimati <aly-bora.ulusoy@polytechnique.edu>
2023-01-12 14:20:37 +0100

    halp

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 1;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '224285905063079767849671193631291176814'                       // used to check whether the localStorage data is stale
+var gzip_hash = '223602210798159571115909687569126411886'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
