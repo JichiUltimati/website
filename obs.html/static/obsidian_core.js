f9bb5584bbc69071831d8d9ce6b7637a38a08385
JichiUltimati <aly-bora.ulusoy@polytechnique.edu>
2023-07-19 19:19:28 +0200

    minor modifications

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 1;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '298195522046353134684435831620589980517'                       // used to check whether the localStorage data is stale
+var gzip_hash = '64632612818839195666494271909394107444'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
